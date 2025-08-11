31f5075eb424a7f03753806207b42ae8c5f120b6
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2025-08-12 01:10:43 +0430

    new files added

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+Author: Safiullah Arian 65605444+safiullah-arian@users.noreply.github.com
+Date: 2025-02-17 11:03:21
+LastEditors: Safiullah Arian 65605444+safiullah-arian@users.noreply.github.com
+LastEditTime: 2025-02-18 10:22:55
+FilePath: customization/snooker_reservation/__init__.py
+Description: 这是默认设置,可以在设置》工具》File Description中进行配置
+"""
+from . import models
+# from . import wizard
